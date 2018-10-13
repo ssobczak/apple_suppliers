c01bc5c4f2ca403294ddeb477c334714554d50e0
Szymon Sobczak <szymon.sobczak@gmail.com>
2018-10-13 12:06:17 +0200

    publish docs

--- scripts/export_notebooks.sh
+++ scripts/export_notebooks.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-jupyter nbconvert *.ipynb --output-dir html
+rm -rf docs/*
+
+jupyter nbconvert *.ipynb --output-dir docs
+
+ls docs | grep -v 'index.html' | \
+perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > docs/index.html
